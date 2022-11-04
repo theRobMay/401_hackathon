691016d28620d5145b43c10a4e1dda85b343e6f1
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2022-11-03 20:30:52 -0500

    all css and html changes

--- hn-checkpoint/src/App.js
+++ hn-checkpoint/src/App.js
@@ -8,8 +8,8 @@ import Articles from "./articles";
 
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [search, setSearch] = useState('');
+    const [articles, setArticles] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch(`http://hn.algolia.com/api/v1/search?query=`+ search)
@@ -17,87 +17,88 @@ function App() {
             .then((data) => {
                 setArticles(data.hits);
             });
-        }, [search]);
-    
-
-  useEffect(() => {
-    console.log("updated", articles);
-  }, [articles]);
+    }, [search]);
 
 
-  return (
-    <div className="App">
-      <div className="default">
-        <div className="container">
-            <header className="searchHeader_container">
-                <div className="searchHeader_search">
+    useEffect(() => {
+        console.log("updated", articles);
+    }, [articles]);
+
+
+    return (
+        <div className="App">
+            <div className="default">
+                <div className="container">
+                    <header className="searchHeader_container">
+                        <div className="searchHeader_search">
                     <span className="searchHeader_logo">
                         <a href="https://news.ycombinator.com">
                             <img src="https://d1sz9gun5ag95e.cloudfront.net/packs/media/images/logo-hn-search-a822432b.png" alt="HackerNewsLogo"></img>
                         </a>
                     </span>
-                        <span className="searchHeader_label">
+                            <span className="searchHeader_label">
                             <h2>Search</h2>
                             <h2>Hacker News</h2>
                         </span>
-                    <span>
+                            <span>
                         <button className ="glass">
                             <img className ="magnifying" src="https://icones.pro/wp-content/uploads/2021/06/icone-loupe-orange.png" alt="magnifying"></img>
                         </button>
                     </span>
 
-                        <input className = "searchInput" type="search" placeholder="Search stories by title, url or author" onChange={event => setSearch(event.target.value)} value = {search}></input>
+                            <input className = "searchInput" type="search" placeholder="Search stories by title, url or author" onChange={event => setSearch(event.target.value)} value = {search}></input>
 
-                    <span>
+                            <span>
                         <img className="fa fa-cog"  src="https://img.icons8.com/external-anggara-basic-outline-anggara-putra/24/000000/external-setting-basic-user-interface-anggara-basic-outline-anggara-putra.png" alt="settings"></img>
                     </span>
-                    <span>
+                            <span>
                             <p className="settings">Settings</p>
                     </span>
 
+                        </div>
+                    </header>
+
                 </div>
-            </header>
-
-        </div>
 
 
-        <div className="appBody">
-            <div className = "dropDownMenuContainer">
+                <div className="appBody">
+                    <div className = "dropDownMenuContainer">
                 <span className="dropDownSearch">
                     Search
 
                 </span>
+                    </div>
+
+                    <ul className="articles">
+                        {articles.map(article => {
+                            return <Articles data={article}/>
+                        })
+                        }
+                    </ul>
+                </div>
             </div>
 
-            <ul className="articles">
-            {articles.map(article => {
-              return <Articles data={article}/>
-            })}
-            </ul>
+            <footer className="footer_container">
+                <ul>
+                    <li className ="footer">
+                        <button className="about" >About</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="settingFooter" >Setting</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="help" >Help</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="api" >API Documentation</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="hackerNews" >Hacker News</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="fork" >Fork/Contribute</button>
+                        <li className="Interpunct"> · </li>
+                        <button className="coolApps" >Cool Apps</button>
+                    </li>
+                </ul>
+            </footer>
         </div>
-      </div>
-
-      <footer className="footer_container">
-            <ul>
-                <li className ="footer">
-                    <button className="about" >About</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="settingFooter" >Setting</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="help" >Help</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="api" >API Documentation</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="hackerNews" >Hacker News</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="fork" >Fork/Contribute</button>
-                        <li className="Interpunct"> · </li>
-                    <button className="coolApps" >Cool Apps</button>
-                </li>
-            </ul>
-        </footer>
-    </div>
-  );
+    );
 }
 
 export default App;
