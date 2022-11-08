cf6ed9e66edf338be10fc82953e044957a78bb87
theRobMay <robbie.maysent@gmail.com>
2022-11-08 16:16:46 -0600

    styling li

--- hn-checkpoint/src/articles.js
+++ hn-checkpoint/src/articles.js
@@ -4,7 +4,7 @@ import "./App.css";
 export default function Articles(props){
     return(
       <li className={"articleList"}>
-          <h4 className={"articleTitle"}>{props.data.title} <a href={props.data.url} target="_blank" rel="noopener noreferrer">{props.data.url}</a>
+          <h4 className={"articleTitle"}>{props.data.title} <a href={props.data.url} target="_blank" rel="noopener noreferrer">({props.data.url})</a>
           </h4>
           <p className={"articlePoints"}>Points: {props.data.points} | Author: {props.data.author}</p>
 
